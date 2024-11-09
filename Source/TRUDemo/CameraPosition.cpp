7e7a402501ccbae2b6d2bf1cc36c3d1abfb20af0
williluk <146780724+williluk@users.noreply.github.com>
2024-11-08 19:38:19 -0800

    Migrated integration layer from old project
    The CameraPosition and CameraMover components have been imported, as well as the associated blueprints and an environment made of basic assets. I tested it to make sure it behaves as-intended, but let me know if you experience any issues.

---
+++ Source/TRUDemo/CameraPosition.cpp
@@ -0,0 +1,66 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CameraPosition.h"
+
+// Sets default values for this component's properties
+UCameraPosition::UCameraPosition()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+template<class T>
+static T* GetComponentByClass(AActor* Actor)
+{
+	// Checks whether actor is valid before trying to get component
+	return Actor ? Cast<T>(Actor->GetComponentByClass(T::StaticClass())) : nullptr;
+}
+
+
+// Called when the game starts
+void UCameraPosition::BeginPlay()
+{
+	Super::BeginPlay();
+	if (IsValid(connectedCameraPosition)) {
+		
+		if (GetComponentByClass<UCameraPosition>(connectedCameraPosition)->bindPath(connectionInputDirection, this) == 0) {
+
+			bindPath(5 - connectionInputDirection, GetComponentByClass<UCameraPosition>(connectedCameraPosition));
+		}
+		else {
+			UE_LOG(LogTemp, Warning, TEXT("UCameraPosition could not be detecte on connectedCameraPosition"));
+		}
+	} 
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("connected Camera Position is null pointer: from CameraPosition %s"), *stateLabel);
+
+	}
+	
+}
+
+
+// Called every frame
+void UCameraPosition::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
+int UCameraPosition::bindPath(int dir, UCameraPosition* bindPos) {
+	if (connectionsList.Contains(FString::FromInt(dir))) {
+
+		UE_LOG(LogTemp, Warning, TEXT("Bind dir already bound. try a different one"));
+		return 1;
+
+	} else {
+		UE_LOG(LogTemp, Warning, TEXT("Bind attempted: %s to %s"), *stateLabel, *(bindPos->stateLabel));
+		connectionsList.Add(FString::FromInt(dir), bindPos);
+		return 0;
+	}
+}
+
