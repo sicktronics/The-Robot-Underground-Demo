31479ad1ff58f89d950cae1de39a027de166f4ea
Him Shun Lee <doeua157@gmail.com>
2025-03-04 22:39:49 -0800

    Revert "Adding Arduino Simulation library (LED Blink Test 1)"

--- Source/TRUDemo/TRUDemo.Build.cs
+++ Source/TRUDemo/TRUDemo.Build.cs
@@ -1,6 +1,5 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
-using System;
-using System.IO;
+
 using UnrealBuildTool;
 
 public class TRUDemo : ModuleRules
@@ -9,12 +8,9 @@ public class TRUDemo : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		// Explicitly including the LEDBlinkTest plugin module here
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "LEDBlinkTest" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
 
-		// Explicitly including the LEDBlinkTest plugin module here
-		// LEDBlinkTest internally includes the static library per its own .Build.cs
-		PrivateDependencyModuleNames.AddRange(new string[] { "LEDBlinkTest" });
+		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
@@ -23,6 +19,5 @@ public class TRUDemo : ModuleRules
 		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
 		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
-		
 	}
 }
